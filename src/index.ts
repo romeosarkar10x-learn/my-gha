5f89d5bb4074a193357b36b3ac47447fcc42e778
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-01-18 21:37:13 +0530

    Updated github action

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import core from "@actions/core";
 
 async function main(): Promise<void> {
     core.info("Hello world from 'my-gha'!");
+
+    const name = core.getInput("name", { required: true });
+    const programmingLanguages = core.getInput("languages", { required: true });
+
+    core.info(`Hi ${name},`);
+    core.info(`I see you are proficient in ${programmingLanguages.split(",")}`);
 }
 
 main();
